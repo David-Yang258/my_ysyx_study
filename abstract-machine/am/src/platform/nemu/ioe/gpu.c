e9300f89f447fdd681d2114a88cdfd3b949aa145
241220000-Hajimi <strin6_yyx@163.com>
2026-04-14 17:57:30 +0800

    Stage C pre done

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,14 +17,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-	if (!ctl->sync && (w == 0 || h == 0)) return;
+	int x = ctl->x, y = ctl->y;
+	int w = ctl->w, h = ctl->h;
+	//Row first
 	uint32_t *pixels = ctl->pixels;
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+	uint32_t screen_width = inl(VGACTL_ADDR) >> 16;
 	for (int i = y; i < y+h; i++) {
 	     for (int j = x; j < x+w; j++) {
-		        fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+		        outl(FB_ADDR + 4*(i * screen_width + j), pixels[w*(i-y)+(j-x)]);
 			    }
 	    }
 	if (ctl->sync) {
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -74,6 +74,8 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+
+  // SYNC_ADDR = VGACTL_ADDR + 4
   uint32_t sync = vgactl_port_base[1];
   if(sync){
 	update_screen();
